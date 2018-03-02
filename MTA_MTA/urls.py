3223439ce4675c4a7dbd1c39be56f01ff37a97e7
mporter-gre <m.porter@dundee.ac.uk>
2018-03-02 12:14:22 +0000

    Working example

--- MTA_MTA/urls.py
+++ MTA_MTA/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('django.views.generic.simple',
      # index 'home page' of the appname e.g. webtest
      url(r'^$', views.index, name='<appname>_index'),
 
+     url(r'^stack_preview/(?P<imageId>[0-9]+)/$', views.stack_preview,
+     name="<appname>_stack_preview")
+
  )
--- MTA_MTA/views.py
+++ MTA_MTA/views.py
@@ -1,7 +1,22 @@
 from django.http import HttpResponse
+from omeroweb.webclient.decorators import login_required
+from django.shortcuts import render
 
 def index(request):
     """
     Just a place-holder while we get started
     """
-    return HttpResponse("Welcome to your app home-page!")
+    return HttpResponse("Welcome to your app home-page! adsfasdfasdf")
+
+
+@login_required()
+def stack_preview(request, imageId, conn=None, **kwargs):
+     """ Shows a subset of Z-planes for an image """
+     image = conn.getObject("Image", imageId)
+     image_name = image.getName()
+     size_z = image.getSizeZ()
+     z_indexes = [0, int(size_z*0.25), int(size_z*0.5),
+          int(size_z*0.75), size_z-1]
+     return render(request, 'MTA_MTA/stack_preview.html',
+           {'imageId': imageId, 'image_name': image_name,
+            'z_indexes': z_indexes})
