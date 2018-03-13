2c9cc7f0975d0dbe061b736ad73409ef6849d45a
Michael Porter <mporter@LS31830.dyn.lifesci.dundee.ac.uk>
2018-03-13 18:46:53 +0000

    Env

--- MTA_MTA/views.py
+++ MTA_MTA/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from omeroweb.webclient.decorators import login_required
 from django.shortcuts import render
 
+
 def index(request):
     """
     Just a place-holder while we get started
@@ -11,12 +12,11 @@ def index(request):
 
 @login_required()
 def stack_preview(request, imageId, conn=None, **kwargs):
-     """ Shows a subset of Z-planes for an image """
-     image = conn.getObject("Image", imageId)
-     image_name = image.getName()
-     size_z = image.getSizeZ()
-     z_indexes = [0, int(size_z*0.25), int(size_z*0.5),
-          int(size_z*0.75), size_z-1]
-     return render(request, 'MTA_MTA/stack_preview.html',
-           {'imageId': imageId, 'image_name': image_name,
-            'z_indexes': z_indexes})
+    """ Shows a subset of Z-planes for an image """
+    image = conn.getObject("Image", imageId)
+    image_name = image.getName()
+    size_z = image.getSizeZ()
+    z_indexes = [0, int(size_z*0.25), int(size_z*0.5), int(size_z*0.75), size_z-1]
+    return render(request, 'MTA_MTA/stack_preview.html',
+        {'imageId': imageId, 'image_name': image_name,
+        'z_indexes': z_indexes})
