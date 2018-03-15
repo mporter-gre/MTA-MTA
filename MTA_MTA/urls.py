254596691c42f7577201f7e8db9da20caa88c1c3
Michael Porter <mporter@LS31830.local>
2018-03-15 01:18:28 +0000

    Setup

--- MTA_MTA/urls.py
+++ MTA_MTA/urls.py
@@ -6,7 +6,8 @@ urlpatterns = patterns('django.views.generic.simple',
      # index 'home page' of the appname e.g. webtest
      url(r'^$', views.index, name='<appname>_index'),
 
-     url(r'^stack_preview/(?P<imageId>[0-9]+)/$', views.stack_preview,
-     name="<appname>_stack_preview")
+     url(r'^stack_preview/(?P<imageId>[0-9]+)/$', views.stack_preview, name="<appname>_stack_preview"),
+
+     url(r'^test_template', views.test_template)
 
  )
--- MTA_MTA/views.py
+++ MTA_MTA/views.py
@@ -16,7 +16,12 @@ def stack_preview(request, imageId, conn=None, **kwargs):
     image = conn.getObject("Image", imageId)
     image_name = image.getName()
     size_z = image.getSizeZ()
-    z_indexes = [0, int(size_z*0.25), int(size_z*0.5), int(size_z*0.75), size_z-1]
+    z_indexes = [0, int(size_z*0.25), int(size_z*0.5), int(size_z*0.75),
+                 size_z-1]
     return render(request, 'MTA_MTA/stack_preview.html',
-        {'imageId': imageId, 'image_name': image_name,
-        'z_indexes': z_indexes})
+                  {'imageId': imageId, 'image_name': image_name,
+                   'z_indexes': z_indexes})
+
+
+def test_template(request):
+    return render(request, 'MTA_MTA/test_template.html')
